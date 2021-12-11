357842c622a3497b452eb1b7322d847b2b5e9b1b
Irfan Farooq <irfandominate@gmail.com>
2021-12-11 16:45:36 +0530

    added dark mode

--- src/App.js
+++ src/App.js
@@ -3,14 +3,42 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react'
+import Alert from './components/Alert';
+
 
 function App() {
+  const [mode, setMode] = useState('light'); //wheather dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#0c4f6f';
+      showAlert("Dark mode has been enabled", "success")
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Lightmode has been enabled", "success")
+    }
+  }
   return (
     <>
-      <Navbar title="Text Utils" aboutText="About" />
+      <Navbar title="Text Utils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
         {/* <About /> */}
-        <TextForm heading="Enter the text to analyze below" />
+        <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} toggleMode={toggleMode} />
       </div>
     </>
   );
